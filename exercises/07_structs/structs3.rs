54a4899d64c59b38e322a289f033d4cbd28613ed
Sedjro2002 <habibhbn3@gmail.com>
2024-01-29 17:18:34 +0100

    strings done

--- exercises/07_structs/structs3.rs
+++ exercises/07_structs/structs3.rs
@@ -7,7 +7,7 @@
 // Execute `rustlings hint structs3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 #[derive(Debug)]
 struct Package {
@@ -31,12 +31,16 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        if self.sender_country == self.recipient_country {
+            false
+        } else {
+            true
+        }
     }
 
-    fn get_fees(&self, cents_per_gram: u32) -> ??? {
-        // Something goes here...
+    fn get_fees(&self, cents_per_gram: u32) -> i64 {
+        (self.weight_in_grams as i64) * (cents_per_gram as i64)
     }
 }
 
